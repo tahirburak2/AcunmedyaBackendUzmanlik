1c9bafa1d97739dd09a77f9b1aa090c5468de2cb
tahirburak2 <63349484+tahirburak2@users.noreply.github.com>
2024-12-07 15:01:00 +0300

    ilk gün

---
+++ 01-İleri seviye c# Proglamlama/week1/Proje02_Var_Dynamic/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+var anahtar kelimesi tür çıkarımı (type inference) sağlayan bir kelimedir.
+derleyici değişkenin türünü atanan değerden bakarak otomaitk olarak belirler derleme zamanında bu tür sabit bir şekilde belirlenmiş olur.
+
+-var kullanımı türün açık bir şekilde belli olduğu durumlarda kullanılırsa daha sağlıklı olur.
+-tür bir kez belirlendikten sonra değiştirilemez
+*/
+
+var number = 5;
+var name = "Ali";
+string address;
+address="İstanbul";
+
+//var price;// yanlış kullanım 
+
+/*
+dynamic derleme zamanında tür kontrolü yapmadan çalışma zamanına geçerek orada bu çıkarsamanın yapılmasını sağlar bu yüzden daha esnek bir yapı sunarken , hatalara da daha açık bir yapıdır
+*/
+
+// var a=5;
+// a="burak";
+
+dynamic a=5;
+a="Engin";
